622f6e13ec72f478bf85b29b7d501a22c8214a65
EslmeOuederni <sousououederni@gmail.com>
2025-08-18 23:54:00 +0100

    Implement CRUD functionality for companies with views for index, show, new, and edit

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,4 +1,40 @@
 class CompaniesController < ApplicationController
+    before_action :set_product, only: %i[ show edit update ]
   def index
+    @companies = Company.all
   end
+  def show
+    @company = Company.find(params[:id])
+  end
+   def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new(company_params)
+    if @company.save
+      redirect_to @company
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @company.update(company_params)
+      redirect_to @company
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  private
+    def set_product
+      @product = Product.find(params[:id])
+    end
+    def company_params
+      params.expect(company: [ :name, :website ])
+    end
+
 end
